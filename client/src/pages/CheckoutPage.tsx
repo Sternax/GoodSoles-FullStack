54139f26fc3ef8c088a478ab8f024808f251d7d0
Sternax <136315680+Sternax@users.noreply.github.com>
2025-05-13 13:40:39 +0200

    Changed files:

---
+++ client/src/pages/CheckoutPage.tsx
@@ -0,0 +1,9 @@
+const CheckoutPage = () => {
+  return (
+    <div>
+      <h1>Checkout Page</h1>
+      <p>Checkout page content goes here.</p>
+    </div>
+  );
+};
+export default CheckoutPage;
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple home page.</p>
+    </div>
+  );
+};
+export default HomePage;
