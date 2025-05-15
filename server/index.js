b4ef7b5d9428a6b470ec3a5b31cf53c1b4a921cf
Sternax <136315680+Sternax@users.noreply.github.com>
2025-05-15 13:41:35 +0200

    Added sqlite module + configured database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,27 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const sqlite = require('sqlite');
+
+let database;
+(async () => {
+  database = await sqlite.open({
+    driver: sqlite3.Database,
+    filename: './database.sqlite3',
+  });
+
+  await database.run('PRAGMA foreign_keys = ON;');
+  console.log('Connected to the SQLite database.');
+})();
+
 const app = express();
-app.use(express.json());
-app.listen(3001, () => console.log('Server running on port 3001'));
+
+app.get('/', (req, res) => {
+  database.all('SELECT * FROM productTable').then((productTable) => {
+    res.send(productTable);
+    console.log('Product table:', productTable);
+  });
+});
+
+app.listen(8080, () =>
+  console.log('Server running on port http://localhost:8080/'),
+);
