5b0fbfc75f7c13ca449baef07110b11463ae4764
Sternax <136315680+Sternax@users.noreply.github.com>
2025-05-27 11:45:23 +0200

    Style LoginSuccessPage with CSS for improved layout and button design

--- client/src/pages/LoginSuccessPage.tsx
+++ client/src/pages/LoginSuccessPage.tsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+import './LoginSuccessPage.css';
+
 const LoginSuccessPage = () => {
   return (
-    <div>
+    <div className="loginSuccess">
       <h1>LOGIN SUCCESSFULL</h1>
+      <Link to="/login">
+        <button>SIGN IN</button>
+      </Link>
     </div>
   );
 };
